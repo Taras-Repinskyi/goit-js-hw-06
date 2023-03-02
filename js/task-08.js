0d5066c2da5ae373470009bd7895ccf4faeb1284
Taras-Repinskyi <repinskiy.taras@gmail.com>
2023-03-02 22:47:46 +0200

    finish task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit',onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+     
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+        console.log('onFormSubmit -> name', name);
+        console.log('onFormSubmit -> value', value);
+   
+        if (name = '' || value === '') {
+            window.alert('Всі поля мають бути заповнені!');
+             event.target.reset();
+        }
+    });
+};
