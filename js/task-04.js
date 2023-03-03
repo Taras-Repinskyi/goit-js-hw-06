aee5cf2ebdd7b535dcd6975e79fe6b8bfd7db2bf
Taras-Repinskyi <repinskiy.taras@gmail.com>
2023-03-03 21:16:21 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,7 @@
 const decrementBtnEl = document.querySelector('[data-action="decrement"]');
 const incrementBtnEl = document.querySelector('[data-action="increment"]');
 const spanEl = document.querySelector('#value');
-
+const count = 0;
 
     
 
@@ -10,6 +10,6 @@ decrementBtnEl.addEventListener('click', () => {
     spanEl.textContent -= 1;
 });
 incrementBtnEl.addEventListener('click', () => {
-    let counterValue = parseInt(spanEl.textContent);
-    spanEl.textContent = counterValue += 1;
+    let count = parseInt(spanEl.textContent);
+    spanEl.textContent = count += 1;
 });
